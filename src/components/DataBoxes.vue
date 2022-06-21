7f00cfb1f325d424bef7c5d52861e268606fadfe
Moses Fawole <mosesfawole2001@gmail.com>
2022-06-21 17:55:15 +0100

    Created header

---
+++ src/components/DataBoxes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="grid md:grid-cols-2 gap-4">
+    <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ formatNumber(stats.NewConfirmed) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ formatNumber(stats.TotalConfirmed) }}
+      </div>
+    </div>
+    <!-- Box 2 -->
+    <div class="shadow-md bg-blue-200 p-10 text-center rounded">
+      <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">New:</span>
+        {{ formatNumber(stats.NewDeaths) }}
+      </div>
+      <div class="text-2xl mb-4">
+        <span class="font-bold">Total:</span>
+        {{ formatNumber(stats.TotalDeaths) }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "DataBoxes",
+  props: ["stats"],
+  methods: {
+    formatNumber(x) {
+      return x.toLocaleString();
+    },
+  },
+};
+</script>
