172d49dc453a299c4e16d1d997d0c2c655a36814
Moses Fawole <mosesfawole2001@gmail.com>
2022-06-21 18:07:14 +0100

    Countries selection completed

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -5,11 +5,7 @@
     class="form-select block w-full border p-3 rounded mt-10"
   >
     <option value="0">Select Country</option>
-    <option
-      v-for="country in countries"
-      v-bind:key="country.ID"
-      :value="country.ID"
-    >
+    <option v-for="country in countries" :value="country.ID">
       {{ country.Country }}
     </option>
   </select>
@@ -20,10 +16,15 @@ export default {
   name: "CountrySelect",
   props: ["countries"],
   data() {
-    console.log(this.Country);
     return {
       selected: 0,
     };
+  },
+  methods: {
+    onChange() {
+      const country = this.countries.find((item) => item.ID === this.selected);
+      this.$emit("get-country", country);
+    },
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
 
     <DataBoxes :stats="stats" />
 
-    <CountrySelect :countries="countries" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else="">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -38,6 +38,10 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
     },
   },
   async created() {
